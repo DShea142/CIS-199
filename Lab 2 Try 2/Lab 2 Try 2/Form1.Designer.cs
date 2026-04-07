6786103c901eb3eff91eeaf9f17601000d2e7418
DShea142 <d.shea14@outlook.com>
2026-04-07 19:13:24 -0400

    Add files via upload

---
+++ Lab 2 Try 2/Lab 2 Try 2/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Lab_2_Try_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "20%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "15%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "18%";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 50);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 50);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(374, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 50);
+            this.label6.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(393, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate Tip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(362, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(198, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Enter price of meal:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 256);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab 2 Try 2/Lab 2 Try 2/Form1.cs
@@ -0,0 +1,52 @@
+﻿// CIS-199-01
+// Lab 2
+// Due 9/17/2023
+// By: S2325
+// This Program Shows Tips for different money amounts
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2_Try_2
+{
+    public partial class Form1 : Form
+    {
+
+        //Setting the constants for the tip rates 
+        private const decimal TIP_RATE_LOW = 0.15m; //15%
+        private const decimal TIP_RATE_MEDIUM = 0.18m; //18%
+        private const decimal TIP_RATE_HIGH = 0.20m; //20%
+        decimal TotalAmount;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //This is where I will get the total amount variable to use later
+            TotalAmount = int.Parse(textBox1.Text);
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //This is where I will calculate each tip percentage
+            decimal TotAmtHigh = TotalAmount * TIP_RATE_HIGH; //Calcualting tip amount 
+            label6.Text = TotAmtHigh.ToString(); //running the amount into the form
+            decimal TotAmtMid = TotalAmount * TIP_RATE_MEDIUM; //Calcualting tip amount 
+            label5.Text = TotAmtMid.ToString(); //running the amount into the form
+            decimal TotAmtLow = TotalAmount * TIP_RATE_LOW; //Calcualting tip amount 
+            label4.Text = TotAmtLow.ToString(); //running the amount into the form
+        }
+
+    }
+}
